30d00fe090296bd5a5f4214bb7fc80d0e19463ee
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-12-05 00:30:04 -0300

    formas geometricas

--- pagame.py
+++ pagame.py
@@ -1,5 +1,13 @@
 import pygame,sys
 pygame.init()
+
+#colores
+negro=(0,0,0)
+blanco=(255,255,255)
+rojo=(255,0,0)
+verde=(0,255,0)
+azul=(0,0,255)
+
 tamanio=(800,400)
 ventanita=pygame.display.set_mode(tamanio)
 
@@ -8,4 +16,12 @@ while True:
         print(i)
         if i.type == pygame.QUIT:
             sys.exit()
+
+    ventanita.fill(azul)
+    pygame.draw.line(ventanita,rojo,[50,200], [200,200], 8)
+    pygame.draw.rect(ventanita,negro,(100,100,40,40))
+    pygame.draw.circle(ventanita,blanco,(300,100),20)
+    pygame.draw.polygon(ventanita,verde,[(500,100),(550,150),(600,50)])
+
+    pygame.display.flip()
 
