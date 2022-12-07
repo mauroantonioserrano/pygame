17771bb2b1a5525cafbbe294f27947d0865bd1bd
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-12-06 23:33:29 -0300

    imagen del jugador que sigue al mouse

--- fondo_imagen.py
+++ fondo_imagen.py
@@ -8,12 +8,20 @@ ventana=pygame.display.set_mode(dimension)
 cerrar=False
 
 fondo=pygame.image.load("paisaje.jpg").convert()
+jugador=pygame.image.load("descarga.png")
+jugador.set_colorkey(blanco)
+
+pygame.mouse.set_visible(0)
 
 while not cerrar:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             cerrar=True
+    mouse=pygame.mouse.get_pos()
+    x=mouse[0]
+    y=mouse[1]
     ventana.blit(fondo,[-20,-200])
+    ventana.blit(jugador,[x,y])
 
     pygame.display.flip()
 
