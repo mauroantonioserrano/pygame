7c8e7114d32c5bc105ca450f72353e2f82ddb260
mauroantonioserrano <maurocerano@gmail.com>
2022-12-07 00:14:52 -0300

    agregamos una imagen de walle con movimiento como icono de player que sigue al movimiento del mouse

--- imagen_de_fondo.py
+++ imagen_de_fondo.py
@@ -5,14 +5,19 @@ pygame.init()
 
 tamanio=(736,414)
 azul=(0,0,255)
+blanco=(255,255,255)
 rojo=(255,0,0)
+negro=(0,0,0)
 ventana=pygame.display.set_mode(tamanio)
 
 fondo=pygame.image.load("6ea1a3ddc80612b8997fb20bc0223f6a.jpg").convert()
+jugador=pygame.image.load("pngegg.png").convert()
+jugador.set_colorkey(negro)
 reloj=pygame.time.Clock()
 x=100
 y=200
 velocidad=0
+pygame.mouse.set_visible(0)
 
 while True :
     for i in pygame.event.get():
@@ -28,9 +33,13 @@ while True :
                 velocidad=0
             elif i.key==pygame.K_RIGHT:
                 velocidad=0
+    mouse=pygame.mouse.get_pos()
+    x=mouse[0]
+    y=mouse[1]
 
 
     ventana.blit(fondo,[0,0])
+    ventana.blit(jugador,[x,y])
     x+=velocidad
     pygame.draw.circle(ventana,rojo,(x,y),20)
 
